f8e4f47385dd5bc5f7d0f56c7117be520f0e9e2e
Divanshu Agarwal <divanshuagarwal2001@gmail.com>
2020-12-19 14:02:51 +0530

    added follower and following

--- client/src/pages/ProfileFollowers.js
+++ client/src/pages/ProfileFollowers.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Container,
   Paper,
@@ -14,6 +14,7 @@ import img from "../assets/code.svg";
 import ReactMarkdown from "react-markdown";
 import Star from "../assets/star.svg";
 import Rating from "@material-ui/lab/Rating";
+import FollowerCard from "../components/FollowerCard/FollowerCard";
 import { useHistory } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
@@ -101,8 +102,8 @@ const Cards = ({ language, stars }) => {
 };
 
 const Profile = () => {
-  const classes = useStyles();
   const history = useHistory();
+  const classes = useStyles();
   return (
     <Grid
       container
@@ -180,7 +181,6 @@ const Profile = () => {
       </Grid>
       <Grid container item xs={12} lg={8} direction="column">
         <Paper
-          elevation={5}
           style={{
             height: "fit-content",
             borderRadius: 20,
@@ -192,57 +192,11 @@ const Profile = () => {
             color: "white",
           }}
         >
-          <h1>README.md</h1>
-          <ReactMarkdown>
-            ### Hi there 👋 My name is [Aniket
-            Biswas](https://www.linkedin.com/in/aniket-biswas-59394b191/). I'm a
-            Full Stack Web Developer and exploring various other technologies
-            everyday. I am currently pursuing B.E. 💻 at [Thapar Institute of
-            Engineering and Technology](https://thapar.edu)
-            ![](https://komarev.com/ghpvc/?username=aniketbiswas21) Here's more
-            about me - 🔭 I’m currently working on -
-            [Pix](https://github.com/aniketbiswas21/Pix), [Buying Selling
-            Platform](https://github.com/developer-student-club-thapar/buying_selling_devops)
-            and [Thapar
-            App](https://github.com/developer-student-club-thapar/thapar_app) -
-            🌱 I’m currently learning - ReactJS, NodeJS, GraphQL, Firebase,
-            Typescript and Flutter. - 👯 I’m looking to collaborate on - MERN
-            stack projects - 💬 Ask me about - Anything related to web
-            technologies. :smiley: - 📫 How to reach me -
-            aniket.biswas75@gmail.com - 😄 Pronouns - He/Him - ⚡ Fun fact - .
-            The best way to learn programming you ask? Google it
-            :stuck_out_tongue_closed_eyes:
-          </ReactMarkdown>
+          {[1, 1, 1, 1].map((ele, i) => {
+            return <FollowerCard key={i} />;
+          })}
         </Paper>
-        <Paper
-          elevation={5}
-          style={{
-            height: "fit-content",
-            borderRadius: 20,
-            backgroundColor: "transparent",
-            justifyContent: "center",
-            alignItems: "center",
-            padding: "20px 10px",
-            color: "white",
-            marginTop: 40,
-          }}
-        >
-          <h1 style={{ paddingLeft: "50px" }}>Popular Answers</h1>
-          <Container
-            style={{
-              justifyContent: "center",
-              alignItems: "center",
-              display: "flex",
-              flexWrap: "wrap",
-            }}
-          >
-            {data.map((ele, i) => {
-              return (
-                <Cards key={i} language={ele.language} stars={ele.stars} />
-              );
-            })}
-          </Container>
-        </Paper>
+        )
       </Grid>
     </Grid>
   );
