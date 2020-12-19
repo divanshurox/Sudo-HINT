ed7ff8455054e9175f20dcab51b1ee3f6d46bd63
Divanshu Agarwal <divanshuagarwal2001@gmail.com>
2020-12-19 12:42:05 +0530

    added forms

--- client/src/pages/ForumExplore.js
+++ client/src/pages/ForumExplore.js
@@ -1,11 +1,93 @@
-import { Button, Grid } from "@material-ui/core";
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Modal from "@material-ui/core/Modal";
+import { Grid, Button, TextField } from "@material-ui/core";
+
 import ForumExploreCard from "../components/ForumExploreCard/ForumExploreCard";
 import Layout from "../components/Layout/Layout";
 
-const ForumExplore = () => {
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: "absolute",
+    width: "50%",
+    height: "40%",
+    backgroundColor: "#161b22",
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+    borderRadius: 10,
+  },
+  input: {
+    color: "white",
+  },
+}));
+
+export default function SimpleModal() {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+  const [focus, setFocus] = React.useState(true);
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const body = (
+    <div
+      style={{
+        top: 200,
+        left: 300,
+      }}
+      className={classes.paper}
+    >
+      <h2 id="simple-modal-title">Enter the question</h2>
+      <form className={classes.form} noValidate autoComplete="off">
+        <TextField
+          id="outlined-basic"
+          label="Question"
+          variant="outlined"
+          multiline
+          required
+          fullWidth
+          onFocus={() => setFocus(false)}
+          onBlur={() => setFocus(true)}
+          style={{
+            border: focus && "1px solid #fff",
+            borderRadius: 10,
+            color: "white",
+          }}
+          InputProps={{
+            classes: classes.input,
+          }}
+        />
+      </form>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          marginTop: 50,
+        }}
+      >
+        <Button variant="contained" size="large">
+          Post the Question
+        </Button>
+      </div>
+    </div>
+  );
+
   return (
     <Layout>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+      >
+        {body}
+      </Modal>
       <Grid container spacing={2}>
         <Grid item xs={6}>
           <h1
@@ -21,7 +103,7 @@ const ForumExplore = () => {
           </h1>
         </Grid>
         <Grid item xs={6} style={{ textAlign: "right", margin: "auto" }}>
-          <Button variant="contained" size="large">
+          <Button variant="contained" size="large" onClick={handleOpen}>
             Ask a Question
           </Button>
         </Grid>
@@ -32,6 +114,4 @@ const ForumExplore = () => {
       </Grid>
     </Layout>
   );
-};
-
-export default ForumExplore;
+}
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,114 @@
-import { Button, Grid } from "@material-ui/core";
+import { Button, Grid, TextField } from "@material-ui/core";
 import React from "react";
 import HomeCard from "../components/HomeCard/HomeCard";
 import Layout from "../components/Layout/Layout";
+import { makeStyles } from "@material-ui/core/styles";
+import Modal from "@material-ui/core/Modal";
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: "absolute",
+    width: "70%",
+    height: "70%",
+    backgroundColor: "#161b22",
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+    borderRadius: 10,
+  },
+}));
 
 const Home = () => {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+  const [focus, setFocus] = React.useState(true);
+  const [file, setFile] = React.useState(null);
+  const [caption, setCaption] = React.useState("");
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleChange = (event) => {
+    setFile(URL.createObjectURL(event.target.files[0]));
+  };
+
+  const body = (
+    <div
+      style={{
+        top: 100,
+        left: 230,
+      }}
+      className={classes.paper}
+    >
+      <h2 id="simple-modal-title">Upload an image</h2>
+      <div
+        style={{
+          marginBottom: 20,
+        }}
+      >
+        <img
+          src={file}
+          style={{
+            height: 200,
+            width: 200,
+            borderRadius: 10,
+          }}
+        />
+        <input type="file" onChange={handleChange} />
+      </div>
+      <form className={classes.form} noValidate autoComplete="off">
+        <TextField
+          id="outlined-basic"
+          label="Caption"
+          variant="outlined"
+          value={caption}
+          onChange={(e) => setCaption(e.target.value)}
+          multiline
+          required
+          fullWidth
+          onFocus={() => setFocus(false)}
+          onBlur={() => setFocus(true)}
+          style={{
+            border: focus && "1px solid #fff",
+            borderRadius: 10,
+            color: "white",
+          }}
+          InputProps={{
+            classes: classes.input,
+          }}
+        />
+      </form>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          marginTop: 50,
+        }}
+      >
+        <Button variant="contained" size="large">
+          Post the Question
+        </Button>
+      </div>
+    </div>
+  );
+
   return (
     <>
       <Layout>
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="simple-modal-title"
+          aria-describedby="simple-modal-description"
+        >
+          {body}
+        </Modal>
         <Grid container spacing={2}>
           <Grid item xs={6}>
             <h1
@@ -22,7 +124,7 @@ const Home = () => {
             </h1>
           </Grid>
           <Grid item xs={6} style={{ textAlign: "right", margin: "auto" }}>
-            <Button variant="contained" size="large">
+            <Button variant="contained" size="large" onClick={handleOpen}>
               Add a Post
             </Button>
           </Grid>
